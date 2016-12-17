94b61585a2c2cba3ce272c0e6725076d2744f992
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2016-12-17 16:37:08 +0300

    first commit.

---
+++ CallJSONandXMLVb/CallJSONandXMLVb/Member.vb
@@ -0,0 +1,43 @@
+﻿Public Class Member
+
+    Private m_Id As Integer
+    Public Property Id() As Integer
+        Get
+            Return m_Id
+        End Get
+        Set
+            m_Id = Value
+        End Set
+    End Property
+
+    Private m_Name As String
+    Public Property Name() As String
+        Get
+            Return m_Name
+        End Get
+        Set
+            m_Name = Value
+        End Set
+    End Property
+
+    Private m_Salary As Integer
+    Public Property Salary() As Integer
+        Get
+            Return m_Salary
+        End Get
+        Set
+            m_Salary = Value
+        End Set
+    End Property
+
+    Private m_Position As String
+    Public Property Position() As String
+        Get
+            Return m_Position
+        End Get
+        Set
+            m_Position = Value
+        End Set
+    End Property
+
+End Class
---
+++ CallJSONandXMLVb/CallJSONandXMLVb/Program.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Web.Script.Serialization
+
+Module Program
+
+    Sub Main()
+        Dim xmlClient As JSONandXMLVb.JSONandXMLSoapClient = New JSONandXMLVb.JSONandXMLSoapClient
+        Dim teamFromXML = xmlClient.GetTeamFromXML
+        Console.WriteLine("XML'den gelen sonuçlar:")
+        For Each item In teamFromXML
+            Console.WriteLine(item.Id & " " & item.Name & " " & item.Position & " " & item.Salary)
+        Next
+        Console.WriteLine()
+
+        Dim jsonClient As JSONandXMLVb.JSONandXMLSoapClient = New JSONandXMLVb.JSONandXMLSoapClient
+        Dim teamFromJSON = jsonClient.GetTeamFromJSON
+        Dim jss As JavaScriptSerializer = New JavaScriptSerializer
+        Dim members As List(Of Member) = New List(Of Member)
+        members = jss.Deserialize(Of List(Of Member))(teamFromJSON)
+        Console.WriteLine("JSON'dan gelen sonuçlar:")
+        For Each item In members
+            Console.WriteLine(item.Id & " " & item.Name & " " & item.Position & " " & item.Salary)
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
